5e49a3edefad3200ea4eb1b3851a4c4e4ac05f2d
gauravf <jhagaurav268@gmail.com>
2021-04-11 20:34:52 +0530

    initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,178 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/custom_dialogue.dart';
+import 'package:tic_tac_toe/game_buton.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<GameButton> buttonsList;
+  var player1;
+  var player2;
+  var activePlayer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    buttonsList = doInit();
+  }
+
+  List<GameButton> doInit() {
+    player1 = List();
+    player2 = List();
+    activePlayer = 1;
+    var gameButtons = <GameButton>[
+      GameButton(id: 1),
+      GameButton(id: 2),
+      GameButton(id: 3),
+      GameButton(id: 4),
+      GameButton(id: 5),
+      GameButton(id: 6),
+      GameButton(id: 7),
+      GameButton(id: 8),
+      GameButton(id: 9),
+    ];
+    return gameButtons;
+  }
+
+  void playGame(GameButton gb) {
+    setState(() {
+      if (activePlayer == 1) {
+        gb.text = "x";
+        gb.bg = Colors.red;
+        activePlayer = 2;
+        player1.add(gb.id);
+      } else {
+        gb.text = "0";
+        gb.bg = Colors.black;
+        activePlayer = 1;
+        player2.add(gb.id);
+      }
+      gb.enabled = false;
+      checkWinner();
+    });
+  }
+
+  void checkWinner() {
+    var winner = -1;
+    if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+      winner = 2;
+    }
+
+    //row2
+    if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+      winner = 1;
+    }
+    if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+      winner = 2;
+    }
+
+    //row3
+    if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    //col1
+    if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+      winner = 2;
+    }
+
+    //col2
+    if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+      winner = 1;
+    }
+    if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+      winner = 2;
+    }
+
+    if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    //diagonal1
+    if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    //diagonal2
+    if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+      winner = 1;
+    }
+    if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+      winner = 2;
+    }
+
+    if (winner != -1) {
+      if (winner == 1) {
+        showDialog(
+            context: context,
+            builder: (_) => CustomDialog("Player One won",
+                "Press the reset button to Start again", resetGame));
+      } else {
+        showDialog(
+            context: context,
+            builder: (_) => CustomDialog("Player Two won",
+                "Press the reset button to Start again", resetGame));
+      }
+    }
+  }
+
+  void resetGame(){
+    if(Navigator.canPop(context)) Navigator.pop(context);
+    setState(() {
+      buttonsList = doInit();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tic Tac Toe"),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1.0,
+          crossAxisSpacing: 9.0,
+          mainAxisSpacing: 9.0,
+        ),
+        itemCount: buttonsList.length,
+        itemBuilder: (context, i) => SizedBox(
+          width: 100,
+          height: 100,
+          child: RaisedButton(
+            padding: const EdgeInsets.all(8.0),
+            onPressed:
+                buttonsList[i].enabled ? () => playGame(buttonsList[i]) : null,
+            child: Text(buttonsList[i].text,
+                style: TextStyle(color: Colors.white, fontSize: 20.0)),
+            color: buttonsList[i].bg,
+            disabledColor: buttonsList[i].bg,
+          ),
+        ),
+      ),
+    );
+  }
+}
